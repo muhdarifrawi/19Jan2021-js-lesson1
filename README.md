e5078cf37a92c052e2aae76e47e305426017e2cf
muhdarifrawi <muhdarifrawi@gmail.com>
2021-01-19 21:06:41 +0800

    added lines of code and readme.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 19Jan2021-js-lesson1
+
+## Requirements
+
+You must have node.js installed to your local drive.
+
+Please remember to install prompt-sync if you have yet to do so:
+```
+npm install prompt-sync
+```
+to use the prompt, follow this example:
+```
+const prompt = require('prompt-sync')();
+console.log("Hello World")
+let name = prompt("Please enter your name");
+console.log("Hello", name);
+```
---
+++ script.js
@@ -0,0 +1,30 @@
+// equivalent to python's print()
+console.log("good night");
+
+// var variable
+// const constant
+let x = 3;
+x = 4;
+console.log(x);
+
+// snake case this_is_a_sentence
+// camelCaseLooksThisWay 
+let applePie = 3.14;
+let a = false; 
+
+// let s = prompt("say something: ");
+let prompt = require('prompt-sync')();
+console.log("Hello World")
+let name = prompt("Please enter your name: ");
+console.log("Hello", name);
+
+
+// asking for integer
+let i = prompt("give an integer >>> ");
+j = parseInt(i) + 5
+console.log(j);
+
+let arr = [2,4,6,8,10]
+
+console.log(arr);
+console.log(arr[3]);
